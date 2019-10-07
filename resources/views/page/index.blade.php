22e1fdd67fd613867f1d254ef5c84e183e1910ec
alebaviera <baviera.alessandra@gmailcom>
2019-10-07 16:15:59 +0200

    index

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.post-layout')
+@section('content')
+
+<?php foreach ($posts as $post): ?>
+<div class="box">
+  <p>Title: {{$post-> title}}</p>
+  <p>Author: {{$post-> author}}</p>
+  <p>Content: {{$post-> content}}</p>
+
+
+
+      <h5>Category: {{$post-> category -> name}}</h5>
+
+
+</div>
+<?php endforeach; ?>
+@endsection
