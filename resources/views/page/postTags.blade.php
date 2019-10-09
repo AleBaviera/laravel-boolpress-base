6f5f30614831ffd4ecbcd0e5deadc8bd83799ed1
alebaviera <baviera.alessandra@gmailcom>
2019-10-09 18:23:20 +0200

    tags by post

--- resources/views/page/postTags.blade.php
+++ resources/views/page/postTags.blade.php
@@ -1,19 +1,19 @@
 @extends('layout.post-layout')
 @section('content')
 <a href="{{route('pressIndex')}}">BACK</a>
-
+<h1>Post</h1>
 <div class="box">
   <p>Title: {{$post->  title}}</p>
   <p>Author: {{$post->  author}}</p>
   <p>Content: {!!$post->  content!!}</p>
   <h5>Category: {{$post-> category -> name}}</h5>
 </div>
-
+<h1>Tags</h1>
 <?php foreach ($post-> tags as $tag): ?>
   <div class="box">
     <p>Author: {{$tag-> author}}</p>
-    <p>Author: {{$tag-> desc}}</p>
-  </div
+    <p>Description: {{$tag-> desc}}</p>
+  </div>
 <?php endforeach; ?>
 
 @endsection
